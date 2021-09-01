beefb8a06ea680c5ae8a413655ad312588e63514
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-09-02 00:17:30 +0900

    feata: Ninknames part Two

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -25,7 +25,7 @@ socket.addEventListener('close', () => {
 function handleSubmit(event) {
   event.preventDefault();
   const input = messageForm.querySelector('input');
-  socket.send(makeMessage('new message', input.value));
+  socket.send(makeMessage('new_message', input.value));
   input.value = '';
 }
 
--- src/server.js
+++ src/server.js
@@ -19,10 +19,20 @@ const sockets = [];
 
 wss.on('connection', (socket) => {
   sockets.push(socket);
+  socket['nickname'] = 'Anon';
   console.log('Connected to Browser ✔');
   socket.on('close', () => console.log('Disconnected from the browser'));
-  socket.on('message', (message) => {
-    sockets.forEach(aSocket => aSocket.send(message.toString()));
+  socket.on('message', (msg) => {
+    const message = JSON.parse(msg);
+    console.log(message);
+    switch (message.type) {
+      case 'new_message':
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`),
+        );
+      case 'nickname':
+        socket['nickname'] = message.payload;
+    }
   });
 });
 
